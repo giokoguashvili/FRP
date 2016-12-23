aeca9737292a6da73af2da6b3e551d620ac24d22
kogoia <kogo27@gmail.com>
2016-12-23 13:29:23 +0400

    write iterator

---
+++ src/iterator.js
@@ -0,0 +1,28 @@
+let range = {
+    from: 5,
+    to: 19
+}
+
+range[Symbol.iterator] = function() {
+    let current = this.from;
+    let last = this.to;
+    return {
+        next() {
+            if (current <= last) {
+                return {
+                    done: false,
+                    value: current++
+                }
+            }
+
+            return {
+                done: true
+            }
+        }
+    }
+}
+
+for (let item of range)
+    console.log(item);
+console.log(range);
+console.log("done");
